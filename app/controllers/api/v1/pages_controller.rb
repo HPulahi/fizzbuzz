8d8b0f9fd32c66321169f8adc006deb68eab705e
Herj Pulahi <Herjpulahi@gmail.com>
2016-11-14 15:47:20 +0000

    Refactor controller logic into page model

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -2,18 +2,8 @@ class Api::V1::PagesController < ApplicationController
   respond_to :json
 
   def show
-    numbers_array = []
-    page_num  = page_params[:page_num].to_i
-    page_size = page_params[:page_size].to_i
-
-    end_num   = page_num * page_size
-    start_num = (end_num - page_size) + 1
-
-    start_num.upto(end_num) do |i|
-      numbers_array << Item.new(i)
-    end
-
-    render json: numbers_array, status: 200
+    page = Page.for(page_params[:page_num], page_params[:page_size])
+    render json: page, status: 200
   end
 
   private
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,26 @@
-class Page < ApplicationRecord
-  attr_accessor :page_size
+class Page
+  attr_accessor :page_num, :page_size
+
+  def self.for(page_num, page_size)
+    new(page_num, page_size).results
+  end
+
+  def initialize(page_num, page_size)
+    @page_num   = page_num.to_i
+    @page_size  = page_size.to_i
+  end
+
+  def results
+    result_set = []
+
+    end_val   = @page_num * @page_size
+    start_val = (end_val - @page_size) + 1
+
+    start_val.upto(end_val) do |i|
+      result_set << Item.new(i)
+    end
+    result_set
+  end
+
+
 end

823ebeaa8b9362cd09a8fa5d8c0d8e2896da16a8
Herj Pulahi <Herjpulahi@gmail.com>
2016-11-14 10:57:01 +0000

    show actions returns 100 numbers

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -2,8 +2,10 @@ class Api::V1::PagesController < ApplicationController
   respond_to :json
 
   def show
-    puts "show "
-    numbers_array = ["Hello"]
+    numbers_array = []
+    1.upto(100) do |i|
+      numbers_array << i
+    end
     render json: numbers_array, status: 200
   end
 end
--- spec/controllers/api/v1/pages_controller_spec.rb
+++ spec/controllers/api/v1/pages_controller_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Api::V1::PagesController, type: :controller do
       expect(response_body).to be_an(Array)
     end
 
+    it "contains 100 values" do
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body.size).to eq(100) 
+    end
+
   end
 
 end
